1191c6fdf76c73f816a3e8e9b61b2e9a1bd67bb5
bay007 <tnt.galicia@gmail.com>
2025-06-15 17:06:54 -0600

    Change default primary key field type from UUIDField to AutoField

--- fetures/settings.py
+++ fetures/settings.py
@@ -134,4 +134,4 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.UUIDField"
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
