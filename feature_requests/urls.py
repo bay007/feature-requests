9ccebdbe980b70b9d33820f1bf1254f2cd988dab
bay007 <tnt.galicia@gmail.com>
2025-06-15 18:08:00 -0600

    Update URL patterns to use UUID for feature identification

--- feature_requests/urls.py
+++ feature_requests/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('features/new/', views.create_feature, name='create_feature'),
-    path('features/<int:feature_id>/', views.feature_detail, name='feature_detail'),
-    path('features/<int:feature_id>/vote/', views.vote_feature, name='vote_feature'),
+    path('features/<uuid:feature_id>/', views.feature_detail, name='feature_detail'),
+    path('features/<uuid:feature_id>/vote/', views.vote_feature, name='vote_feature'),
 ]
